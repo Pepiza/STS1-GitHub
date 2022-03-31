33836925ccc9ec376e031302f6d380e9703bcb1b
Pepiza <101861250+Pepiza@users.noreply.github.com>
2022-04-01 00:32:09 +0200

    Update TD_GitHub.cpp

--- TD_GitHub.cpp
+++ TD_GitHub.cpp
@@ -1,14 +1,51 @@
 //Fonctions réalisées par l'étudiant 1 :
+#include <iostream>
+using namespace std;
 
+bool estMajeur (int age)
+{
+  
+  bool = majeur;
+  
+  
+ if ( age >=18 )
+  {
+     majeur = true;
+  }
+  else 
+  {
+      majeur = false;
+  }
+  
+  return majeur ;
+  
+}
 
+int exposant ( int nbre , int  exp)
+{
+  
+  int resultat;
+  
+  nbre * exp = resultat ;
+ 
+  
+  return resultat;
 
+}
 
+float TVA(int prix)
+{
+  
+  int TVA;
+  
+  prix * 20/100 = TVA;
+  
+  return TVA
 
-
-
-
-
-
+ }
+int plusGrand(int a , int b, int c)
+{
+}
 //Fonctions réalisées par l'étudiant 2 :
 
 
